bb453995615921d1517a9de5c69783d21774c603
Soufiane CHALOUH <schalouh@leyton.com>
2021-07-24 21:50:19 +0100

    Change css depending on state

--- src/App.js
+++ src/App.js
@@ -26,11 +26,15 @@ function App() {
     const deleteTask = (id) =>{
         setTasks(tasks.filter((task) => task.id !== id))
     }
+    const toggleReminder = (id) => {
+        setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+    }
 
   return (
     <div className="container">
         <Header title='yeo'/>
-        <Tasks tasks={tasks} onDelete={deleteTask}/>
+
+        {tasks.length>0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>: "No tasks to show"}
     </div>
   );
 }
