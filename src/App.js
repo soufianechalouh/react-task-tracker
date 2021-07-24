a6b8643b8ff32f7beefb63a29216cd9333fd43fa
Soufiane CHALOUH <schalouh@leyton.com>
2021-07-24 20:12:44 +0100

    Show tasks as a single component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import {useState} from "react";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+    const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctor appointment',
+        day: 'tomorrow',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'Gym',
+        day: 'today',
+        reminder: false
+    },
+    {
+        id: 3,
+        text: 'Seb appointment',
+        day: 'the day after',
+        reminder: true
+    }
+])
+
   return (
     <div className="container">
         <Header title='yeo'/>
+        <Tasks tasks={tasks}/>
     </div>
   );
 }
---
+++ src/components/Tasks.js
@@ -0,0 +1,10 @@
+
+const Tasks = ({tasks}) => {
+
+    return (
+        <>
+            {tasks.map((task) => (<h3>{task.text}</h3>))}
+        </>
+    )
+}
+export default Tasks
