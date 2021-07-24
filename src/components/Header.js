0bc683d2407345aa4dfb916093962795e9303f01
Soufiane CHALOUH <schalouh@leyton.com>
2021-07-24 19:18:23 +0100

    add css classes

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,9 @@ import PropTypes from 'prop-types'
 
 const Header = ({title}) => {
     return (
-        <header>
+        <header className='header'>
             <h1>{title}</h1>
-            <button>Add</button>
+            <button className='btn'>Add</button>
         </header>
     )
 }
